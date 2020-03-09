4c1081b50c27e10e35f396b8594a72f6894df6da
Jamie Sinclair <jamie.sinclair@postcodelottery.co.uk>
2020-03-09 11:10:52 +0000

    add new page and nav items

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -11,6 +11,9 @@ export default () => (
       <li className={styles.navigationItem}>
         <Link to="/blog/">Blog</Link>
       </li>
+      <li className={styles.navigationItem}>
+        <Link to="/NavPage/">NavTest</Link>
+      </li>
     </ul>
   </nav>
 )
